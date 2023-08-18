fa9cc6321d01409205a04dde2e55dd25d0447e6b
Jake Lohman <jakelohman7@gmail.com>
2023-08-18 10:31:13 -0500

    basic formatting of top tracks for discord embed

--- fetch.py
+++ fetch.py
@@ -11,4 +11,5 @@ base_url = "http://ws.audioscrobbler.com/2.0"
 def weekly_track_chart(username):
     url_suffix = f"/?method=user.getweeklytrackchart&user={username}&api_key={api_key}&format=json"
     res = requests.get(base_url + url_suffix)
-    return json.loads(res.text)
+    output = json.loads(res.text)
+    return output
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import discord
 from dotenv import load_dotenv
 import os
@@ -24,11 +25,40 @@ async def on_message(message):
         return
 
     if message.content.startswith("$lastfm"):
-        await message.channel.send("Getting your data...")
-        username = "jake6969696969"
+        # await message.channel.send("Getting your data...")
 
         # res = weekly_track_chart(username)
-        await message.channel.send("?")
+        tracks = parse_res()
+        embed = create_embed_top_tracks("jake6969696969", tracks)
+
+        await message.channel.send(embed=embed)
+
+
+# Converts weekly track chart response into discord message
+def parse_res():
+    with open("example.json", "r") as res:
+        data = json.load(res)
+        tracklist = data["weeklytrackchart"]["track"][:3]
+        return tracklist
+
+
+# Creates embeds for the discord message
+def create_embed_top_tracks(username, tracks):
+    embed = discord.Embed(
+        title=f"{username}'s Top Tracks Last Week", color=0xD91F11, description="")
+    
+    for track in tracks:
+        title = track["name"]
+        artist = track["artist"]["#text"]
+        # int for coverart size = 0:small, 1:medium, 2:large
+        # but it turns out, lastfm doesn't provide valid coverarts in this response anyway...
+        cover_art = track["image"][1]["#text"]
+        play_count = track["playcount"]
+
+        track_details = f"\n**{title}** by **{artist}** — {play_count} plays\n"
+        embed.description += track_details
+
+    return embed
 
 
 client.run(discord_token)
